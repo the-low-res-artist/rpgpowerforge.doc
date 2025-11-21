5bae9d3190a5ce27285e68cabf657d807008eda6
the-low-res-artist <pixelart.asset@gmail.com>
2025-11-21 18:14:46 +0100

    test

--- rebuild_website.sh
+++ rebuild_website.sh
@@ -42,10 +42,8 @@ for folder in $(find src -mindepth 1 -maxdepth 1 -type d); do
     if [ -f "${folder}/book.toml" ]; then
         # Replace "src" with "book"
         output_dir="${folder/src/book}"
-        echo "mdbook-admonish install ${folder}"
-        mdbook-admonish install ${folder}
-        echo "Building: ${folder} -> ${output_dir}"
-        mdbook build "${folder}" -d "${output_dir}"
+        echo "Building: ${ROOT}/${folder} -> ${ROOT}/${output_dir}"
+        mdbook build "${ROOT}/${folder}" -d "${ROOT}/${output_dir}"
     fi
 done
 
