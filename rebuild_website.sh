e3c821981d9bdbba740cd9489358e04d6153e41b
Gif <76217812+the-low-res-artist@users.noreply.github.com>
2026-04-08 11:59:00 +0200

    Update rebuild_website.sh

--- rebuild_website.sh
+++ rebuild_website.sh
@@ -59,7 +59,7 @@ ln -s ${ROOT}/custom-css        ${ROOT}/book/custom-css
 ln -s ${ROOT}/custom-js         ${ROOT}/book/custom-js
 ln -s ${ROOT}/media             ${ROOT}/book/media
 ln -s ${ROOT}/resources         ${ROOT}/book/resources
-ln -s ${ROOT}/src/doc/news      ${ROOT}/book/news
+ln -s ${ROOT}/src/doc/rpgpowerforge.news      ${ROOT}/book/news
 
 # ---------------------------------------------------------------
 # POST BUILD SCRIPTS
